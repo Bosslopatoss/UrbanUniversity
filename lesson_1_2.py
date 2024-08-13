6c20afb4ffe02a27b5dcf78879d3c702ef5435d9
Bosslopatoss <antonlopatkin62@gmail.com>
2024-08-14 02:22:02 +0900

    Add files via upload

---
+++ lesson_1_2.py
@@ -0,0 +1,5 @@
+all_tasks = 12
+time_for_all_tasks = 1.5
+name_course = 'Python'
+time_for_1_task = time_for_all_tasks / all_tasks
+print("Курс: ", name_course, ", всего задач: ", all_tasks, ", затрачено часов: ", time_for_all_tasks, ", среднее время выполнения: ", time_for_1_task, " часа.", sep = '')
