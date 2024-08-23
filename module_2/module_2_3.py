b62b00ae9e877e9ff42d138b381b1193500f5907
Bosslopatoss <antonlopatkin62@gmail.com>
2024-08-23 23:15:25 +0900

    Update module_2_3.py

--- module_2/module_2_3.py
+++ module_2/module_2_3.py
@@ -1,8 +1,11 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 k = 0
 while k < len(my_list):
-    if my_list[k] >= 0:
+    if my_list[k] > 0:
         print(my_list[k])
+    elif my_list[k] == 0:
+        k += 1
+        continue
     else:
         break
     k += 1
