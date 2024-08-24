1f6d38669210bb7258f5ec87644b1b4cbff60e86
Bosslopatoss <antonlopatkin62@gmail.com>
2024-08-25 00:01:52 +0900

    Update module_2_hard.py

--- module_2/module_2_hard.py
+++ module_2/module_2_hard.py
@@ -3,7 +3,7 @@ result = ''
 for i in range(1, n):
     for j in range(i+1, n):
         divider = i + j
-        if n % divider == 0 and i != j:
+        if n % divider == 0:
             result = result + str(i) + str(j)
 print(result)
 
