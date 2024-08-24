b2c03980d28a8b333e44b21db67346dc54112201
Bosslopatoss <antonlopatkin62@gmail.com>
2024-08-24 23:58:58 +0900

    Update module_2_hard.py

--- module_2/module_2_hard.py
+++ module_2/module_2_hard.py
@@ -1,16 +1,10 @@
 n = int(input())
-list_result = []
 result = ''
 for i in range(1, n):
-    for j in range(1, n):
+    for j in range(i+1, n):
         divider = i + j
         if n % divider == 0 and i != j:
-            list_result.append(str(i)+str(j))
-for i in list_result:
-    for j in list_result:
-        reversed_j = j[::-1]
-        if i == reversed_j:
-            list_result.remove(j)
-for i in list_result:
-    result = result + i
+            result = result + str(i)+str(j)
 print(result)
+
+
