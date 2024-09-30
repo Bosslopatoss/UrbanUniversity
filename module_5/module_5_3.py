0606b0f9c6923a89af7c90245d81bb1c75639bdd
Bosslopatoss <antonlopatkin62@gmail.com>
2024-09-30 21:35:13 +0900

    Update module_5_3.py

--- module_5/module_5_3.py
+++ module_5/module_5_3.py
@@ -3,11 +3,14 @@ class House:
         self.name = name 
         self.number_of_floors = number_of_floors    
     
+    
     def __len__(self):
         return self.number_of_floors
     
+    
     def __str__(self):
         return f'"Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+    
     
     def go_to(self, new_floor):
         if new_floor < 1 or new_floor > self.number_of_floors:
@@ -15,11 +18,14 @@ class House:
         else: 
             for i in range(1, new_floor + 1):
                 print(i)
+    
+    
     def __eq__(self, other):
         if isinstance(other, House):
             return self.number_of_floors == other.number_of_floors
         return False
 
+    
     def __lt__(self, other):
         if isinstance(other, House):
             return self.number_of_floors < other.number_of_floors
